cec55d23360a12c5c90a6ad09dbac82bb993c20f
Noura Khaled <nalmugarri@nouras-macbook-pro.local>
2018-12-03 10:42:29 +0300

    Initial Commit

---
+++ loginApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loginApp
+//
+//  Created by Noura Khaled on 25/03/1440 AH.
+//  Copyright © 1440 Noura Khaled. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
