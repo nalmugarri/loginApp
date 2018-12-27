b070e7687c9d718f368fec8a555ec21e87b5637e
Noura Khaled <nalmugarri@nouras-macbook-pro.local>
2018-12-27 10:56:00 +0300

    log in app

--- loginApp/ViewController.swift
+++ loginApp/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    @IBOutlet weak var message: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        message.isHidden = true
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        
+        guard let enteredEmail = email.text, email.text?.characters.count != 0
+            else {
+                message.isHidden = false
+            message.text = "Please Enter Your Email!"
+                return
+        }
+        
+        guard let password = pass.text, pass.text?.characters.count != 0
+            else {
+                message.isHidden = false
+                message.text = "Please Enter Your Password!"
+                return
+        }
+        
+        message.isHidden = true
+        
+        // he used if statment = " "
     }
 
 
